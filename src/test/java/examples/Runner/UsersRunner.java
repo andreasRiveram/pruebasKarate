6e482c4348af6724e8f62165fa61409490d3826b
ginorivera <ginorivera1115@gmail.com>
2024-11-27 17:12:08 -0500

    ver 2 Maven

--- src/test/java/examples/Runner/UsersRunner.java
+++ src/test/java/examples/Runner/UsersRunner.java
@@ -1,15 +1,20 @@
-package examples;
+package examples.Runner;
 
-import com.intuit.karate.KarateOptions;
-import com.intuit.karate.Results;
+/*import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import static org.junit.jupiter.api.Assertions.*;
-
-import com.intuit.karate.junit5.Karate;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;*/
+
+//import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.KarateOptions;
 import org.junit.jupiter.api.Test;
+import com.intuit.karate.Runner;
+import com.intuit.karate.Results;
+import org.apache.commons.io.FileUtils;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -17,7 +22,9 @@ import java.util.Collection;
 import java.util.List;
 
 @KarateOptions(features = {"classpath:examples/users/users.feature"})
-public class ExamplesTest {
+
+class UsersRunner {
+
     @Test
     public void testParallel() {
         System.out.println(getClass() );
@@ -36,4 +43,5 @@ public class ExamplesTest {
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
+
 }
